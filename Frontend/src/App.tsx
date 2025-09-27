015928356323045c972b990fc78105576b80ce79
Malya-reddy <malyareddy646@gmail.com>
2025-09-27 09:31:33 +0530

    Add files via upload

---
+++ Frontend/src/App.tsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
+import Home from './pages/Home'
+import Courses from './pages/Courses'
+import CourseDetail from './pages/CourseDetail'
+import Quiz from './pages/Quiz'
+import Dashboard from './pages/Dashboard'
+import About from './pages/About'
+import Contact from './pages/Contact'
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-50">
+      <Navbar />
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/courses" element={<Courses />} />
+          <Route path="/course/:id" element={<CourseDetail />} />
+          <Route path="/quiz/:courseId" element={<Quiz />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </main>
+      <Footer />
+    </div>
+  )
+}
+
+export default App
